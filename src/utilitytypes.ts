61210c2103583900610c6a7417cde47f5f55ffbf
matin889 <bornlitematin@gmail.com>
2023-10-12 16:03:37 +0200

    utility types task completed

---
+++ src/utilitytypes.ts
@@ -0,0 +1,53 @@
+interface Menu {
+  name: string;
+  menu: string[];
+  persons?: number;
+}
+
+function updateMenu(menu: Menu, items: Partial<Menu>) {
+  return { ...menu, ...items };
+}
+
+const menu1 = {
+  name: "Breakfast",
+  menu: ["Bread", "Fruits", "Coffee"],
+  persons: 4,
+};
+
+const menu2 = updateMenu(menu1, { persons: 6 });
+// console.log(menu2);
+const menu3 = updateMenu(menu1, {
+  name: "Lunch",
+  menu: ["Rice", "Vegetables"],
+});
+// console.log(menu3);
+
+function showMenu(menu: Required<Menu>) {
+  return menu;
+}
+
+const newMenu = {
+  name: "Dinner",
+  menu: ["Pizza", "Fruits", "Milk"],
+  persons: 8,
+};
+
+// console.log(showMenu(newMenu));
+
+function pickOneItem(menu: Pick<Menu, "name">) {
+  return menu;
+}
+
+const pickName = {
+  name: "Dinner",
+};
+// console.log(pickOneItem(pickName));
+function removeSpecificItem(menu: Omit<Menu, "menu">) {
+  return menu;
+}
+
+const hideMenu = {
+  name: "Dinner",
+  persons: 10,
+};
+console.log(removeSpecificItem(hideMenu));
